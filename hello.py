301b6e313a788b19406dbad3abc134a6fdcc5a08
Michael Gupta <michaelgupta731@gmail.com>
2022-12-24 21:11:00 -0500

    Init commit

---
+++ hello.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+# Create a flask instance
+app = Flask(__name__)
+
+
+#safe
+#capitalize
+#lower
+#upper
+#title
+#trim
+#striptags
+
+
+
+# Create a route decorator
+@app.route('/')
+def index():
+    first_name = "Michael"
+    stuff = "This is <strong>bold text</strong>"
+    favorite_pizza = ["pep","cheese",49]
+    return render_template("index.html",
+        first_name=first_name,
+        stuff=stuff,
+        favorite_pizza=favorite_pizza)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html",user_name=name)
+
+#Create custom error pages
+
+#invalid url
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+#Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
